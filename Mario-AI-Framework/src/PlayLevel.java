ae15313fb0c0aa03bea48b181c708ce8beca7faf
d.ludorff <dominic.ludorff@stud.uni-hannover.de>
2023-05-04 17:33:26 +0200

    refactor and redistribute gen files

--- Mario-AI-Framework/src/PlayLevel.java
+++ Mario-AI-Framework/src/PlayLevel.java
@@ -33,7 +33,7 @@ public class PlayLevel {
 
     public static void main(String[] args) {
         MarioGame game = new MarioGame();
-        // printResults(game.playGame(getLevel("../levels/original/lvl-1.txt"), 200, 0));
-        printResults(game.runGame(new agents.robinBaumgarten.Agent(), getLevel("./levels/original/lvl-1.txt"), 20, 0, true));
+         //printResults(game.playGame(getLevel("../levels/arbitrary_random_samples_v1.00000_h1.00000_st0/txt/0_sc3.txt"), 200, 0));
+        printResults(game.runGame(new agents.robinBaumgarten.Agent(), getLevel("./levels/arbitrary_random_samples_v1.00000_h1.00000_st0/txt/3_sc3.txt"), 20, 0, true));
     }
 }

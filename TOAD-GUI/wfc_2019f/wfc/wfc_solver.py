Fixed wrong right side fixation calculation

TOAD-GUI/wfc_2019f/wfc/wfc_solver.py
@@ -114,6 +114,8 @@ class Solver:
 def makeWave(number_patterns: int, w: int, h: int, lf: int, rf: int, ground: Optional[Iterable[int]] = None, sky: Optional[Iterable[int]] = None,
              bound: Optional[Iterable[int]] = None
              ) -> NDArray[numpy.bool_]:
+    import sys
+    np.set_printoptions(threshold=sys.maxsize)
     wave: NDArray[numpy.bool_] = numpy.ones((number_patterns, w, h), dtype=numpy.bool_)
     if ground is not None:
         wave[:, :, h - 1] = False
@@ -126,16 +128,19 @@ def makeWave(number_patterns: int, w: int, h: int, lf: int, rf: int, ground: Opt
         for s in sky:
             wave[s, :, 0] = True
     if bound is not None:
-        wave[:, :lf, :] = False
-        wave[:, -rf:, :] = False
-        for h, patterns in enumerate(bound):
-            for l, p in enumerate(patterns):
-                # Starting boundaries
-                if l < lf:
-                    wave[p, l, h] = True
-                # Ending Boundaries
-                else:
-                    wave[p, -(len(patterns) - l - 1), h] = True
+        if lf > 0:
+            wave[:, :lf, :] = False
+        if rf > 0:
+            wave[:, -rf:, :] = False
+        if lf+rf:
+            for h, patterns in enumerate(bound):
+                for l, p in enumerate(patterns):
+                    # Starting boundaries
+                    if l < lf:
+                        wave[p, l, h] = True
+                    # Ending Boundaries
+                    else:
+                        wave[p, -(len(patterns) - l), h] = True
     # logger.debug(wave)
     # for i in range(wave.shape[0]):
     #  logger.debug(wave[i])